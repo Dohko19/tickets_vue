0d86c1ee47ec8cb95fbe4bb3e1a02d965a32130b
Daniel Trejo <danielkage9@gmail.com>
2020-04-30 21:42:27 -0500

    fitst commit vue mounted

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Sistema de Tickets</div>
+
+                    <div class="card-body text-center">
+                        <button class="btn btn-info" style="color: white;">Levantar un Ticket</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
